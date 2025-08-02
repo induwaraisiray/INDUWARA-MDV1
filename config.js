6250020b3c630a67a44cdf1984a02fabd1b35948
Imduwara <induwaraisira827@gmail.com>
2025-08-02 09:49:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ STICKER_NAME: process.env.STICKER_NAME || "D-XTRO-MD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "👾, 🧬",
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
