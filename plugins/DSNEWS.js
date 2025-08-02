d880a0d48f993fa0072bab47aad2373e359181ce
Imduwara <induwaraisira827@gmail.com>
2025-08-02 11:43:54 +0530

    Update DSNEWS.js

--- plugins/DSNEWS.js
+++ plugins/DSNEWS.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const { fetchJson } = require('../lib/functions');
 
 const apilink = 'https://nethu-api.vercel.app/news';
-let wm = '> ɪɴᴅᴜᴡᴀʀᴀ 〽️ꜰ';
+let wm = '> ©ᴩᴋᴡᴇʀᴅ ʙʏ ɪɴᴅᴜᴡᴀʀᴀ 〽️ᴅ';
 let latestNews = {};
 let newsInterval = null;
 let alertEnabled = false;
